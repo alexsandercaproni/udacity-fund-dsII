c0ad72ccc781af6d880e7fe26b9579043217ee18
alexsandercaproni <alexsandercaproni@gmail.com>
2019-01-29 21:37:36 -0200

    Desenvolvimento do miniprojeto de cross-validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,8 +14,11 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -25,8 +28,17 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.30, random_state=42)
 
+#Creating model
+clf = tree.DecisionTreeClassifier()
 
-### it's all yours from here forward!  
+#Fitting model
+clf.fit(features_train, labels_train)
 
+#Predict
+pred = clf.predict(features_test)
 
+#Getting accuracy
+accuracy = accuracy_score(labels_test, pred)
+print('Acucaria: ', accuracy)
